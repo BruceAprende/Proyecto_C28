c739d80074c5515f429f65659eb0a78a8702ec97
BruceAprende <90285834+BruceAprende@users.noreply.github.com>
2021-10-15 07:42:38 -0500

    Update launcher.js

--- js/launcher.js
+++ js/launcher.js
@@ -6,6 +6,7 @@ class Launcher{
             stiffness: 0.04,
             length: 10
         }
+        this.bodyA=bodyA;
         this.pointB = pointB
         this.launcher = Constraint.create(options);
         World.add(world, this.launcher);
